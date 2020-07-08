f0fd88966c71c8f597be8284646cbf4fa668c9ad
MaMi <proz4mexx@gmail.com>
2020-07-08 12:42:18 +0300

    4

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -615,7 +615,6 @@
 <script src="https://cdn.jsdelivr.net/npm/jquery.maskedinput@1.4.1/src/jquery.maskedinput.min.js" type="text/javascript"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-<a id="scrollUp" href="#top" style="position: fixed; z-index: 99999999;"><i class="far fa-arrow-alt-circle-up"></i></a>
 <script src="js/vendor/jquery-2.2.4.min.js"></script>
 </body>
 
